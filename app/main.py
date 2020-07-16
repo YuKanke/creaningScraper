468575535788613e1d8b73342762b3492114e1a3
root <root@localhost.localdomain>
2020-07-16 16:54:40 +0900

    スワローチェーン完成

--- app/main.py
+++ app/main.py
@@ -8,10 +8,11 @@ import getKikuya
 import getNomuraCleaning
 import getCleaningRuby
 import getKyokuto
+import getSwallowChain
 
-# functions = {'白洋舎' : getHakuyosha.main, 'クリーニング専科' : getCleaningSenka.main, 'ポニークリーニング' : getPonyCleaning.main, '喜久屋' : getKikuya.main, 'ノムラクリーニング' : getNomuraCleaning.main, 'クリーニングルビー' : getCleaningRuby.main, 'オゾンクリーニングのきょくとう': getKyokuto.main}
+# functions = {'白洋舎' : getHakuyosha.main, 'クリーニング専科' : getCleaningSenka.main, 'ポニークリーニング' : getPonyCleaning.main, '喜久屋' : getKikuya.main, 'ノムラクリーニング' : getNomuraCleaning.main, 'クリーニングルビー' : getCleaningRuby.main, 'オゾンクリーニングのきょくとう': getKyokuto.main, 'スワローチェーン' : getSwallowChain.main}
 
-functions = {'オゾンクリーニングのきょくとう' : getKyokuto.main}
+functions = {'スワローチェーン' : getSwallowChain.main}
 for key, value in functions.items():
     with open('./cleaningShops_' + key + '_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv', 'w', newline='') as f:
         sleep(5)
